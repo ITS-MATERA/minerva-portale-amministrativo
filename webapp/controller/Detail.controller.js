004a82090c079380d339693a890f594ca8d7d95d
Alessio Carone <alessio.carone@innovatesapp.com>
2024-07-22 15:32:02 +0000

    dev: implementato invia

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,24 +1,35 @@
-sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (BaseController, JSONModel) {
-  "use strict";
+sap.ui.define(
+  ["./BaseController", "sap/ui/model/json/JSONModel", "sap/m/MessageBox"],
+  function (BaseController, JSONModel, MessageBox) {
+    "use strict";
 
-  return BaseController.extend("portaleamministrativo.controller.Detail", {
-    onInit: function () {
-      this.getRouter().getRoute("Detail").attachPatternMatched(this._onObjectMatched, this);
-    },
+    return BaseController.extend("portaleamministrativo.controller.Detail", {
+      onInit: function () {
+        this.getRouter().getRoute("Detail").attachPatternMatched(this._onObjectMatched, this);
+      },
 
-    _onObjectMatched: function (oEvent) {
-      var oArguments = oEvent.getParameter("arguments");
+      _onObjectMatched: function (oEvent) {
+        var oArguments = oEvent.getParameter("arguments");
 
-      this.setModel(
-        new JSONModel({
-          Edit: oArguments.Id ? false : true,
-        }),
-        "Item"
-      );
-    },
+        this.setModel(
+          new JSONModel({
+            Edit: oArguments.Id ? false : true,
+          }),
+          "Item"
+        );
+      },
 
-    onBack: function () {
-      this.getRouter().navTo("RouteHome");
-    },
-  });
-});
+      onBack: function () {
+        this.getRouter().navTo("RouteHome");
+      },
+
+      onSend: function () {
+        MessageBox.success(this.getResourceBundle().getText("msgTicketSended"), {
+          onClose: function () {
+            this.getRouter().navTo("RouteHome");
+          }.bind(this),
+        });
+      },
+    });
+  }
+);
