060b97593c327bc03178458c00ad2d475d898477
Alessio Carone <alessio.carone@innovatesapp.com>
2024-07-23 10:02:43 +0000

    dev: aggiunti filtri e  campi lista home

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,24 +1,32 @@
-sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (BaseController, JSONModel) {
-  "use strict";
+sap.ui.define(
+  ["./BaseController", "sap/ui/model/json/JSONModel", "sap/m/Priority"],
+  function (BaseController, JSONModel, Priority) {
+    "use strict";
 
-  return BaseController.extend("portaleamministrativo.controller.Home", {
-    onInit: function () {
-      this.setModel(
-        new JSONModel([
-          {
-            Column1: "Test",
-          },
-        ]),
-        "Items"
-      );
-    },
+    return BaseController.extend("portaleamministrativo.controller.Home", {
+      onInit: function () {
+        this.setModel(
+          new JSONModel([
+            {
+              Ticket: "HDFS_579147",
+              Object: "Ceritificazione unica",
+              State: "Chiuso",
+              CreationDate: new Date(),
+              ClosingDate: new Date(),
+              Priority: "Alta",
+            },
+          ]),
+          "Items"
+        );
+      },
 
-    onDetail: function () {
-      this.getRouter().navTo("Detail", { Id: 1 });
-    },
+      onDetail: function () {
+        this.getRouter().navTo("Detail", { Id: 1 });
+      },
 
-    onNewTicket: function () {
-      this.getRouter().navTo("Detail");
-    },
-  });
-});
+      onNewTicket: function () {
+        this.getRouter().navTo("Detail");
+      },
+    });
+  }
+);
