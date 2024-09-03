5d7f08cf241bbefd74f324f07622ee86997513d8
Alessio Carone <alessio.carone@innovatesapp.com>
2024-09-03 08:30:30 +0000

    dev: implementata nuova maschera ticket

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -6,6 +6,25 @@ sap.ui.define(
     return BaseController.extend("portaleamministrativo.controller.Detail", {
       onInit: function () {
         this.getRouter().getRoute("Detail").attachPatternMatched(this._onObjectMatched, this);
+
+        var oModelSelect = {
+          ItilClassification: [{ Key: "SERVICE REQUEST", Text: "SERVICE REQUEST" }],
+          Conduction: [{ Key: "Tecnica", Text: "Tecnica" }],
+          Environment: [{ Key: "Produzione", Text: "Produzione" }],
+          RequestType: [{ Key: "Supporto specialistisco", Text: "Supporto specialistisco" }],
+          Type: [{ Key: "ASSISTENZA SW", Text: "ASSISTENZA SW" }],
+          Application: [
+            { Key: "MALF-POR", Text: "Malfunzionamento Portale" },
+            { Key: "RICH-INF", Text: "Richiesta di informazioni su Ordine di Pagamento" },
+            { Key: "CERT-UNI", Text: "Certificazione unica" },
+            { Key: "ALTRO", Text: "Altro non specificato in elenco" },
+            { Key: "FATT-BLO", Text: "Fattura bloccata al pagamento" },
+            { Key: "ENTR-MER", Text: "Entrata Merci non presente al portale" },
+            { Key: "QUES-NAT", Text: "Quesito di natura contrattuale/negoziale" },
+          ],
+        };
+
+        this.setModel(new JSONModel(oModelSelect), "Select");
       },
 
       _onObjectMatched: function (oEvent) {
@@ -29,6 +48,14 @@ sap.ui.define(
             this.getRouter().navTo("RouteHome");
           }.bind(this),
         });
+      },
+
+      onApplicationChange: function (oEvent) {
+        var sKey = oEvent.getParameter("selectedItem").getProperty("key");
+
+        if (sKey === "FATT-BLO" || sKey === "ENTR-MER" || sKey === "QUES-NAT") {
+          MessageBox.warning(this.getResourceBundle().getText("msgContactContracualReferent"));
+        }
       },
     });
   }
