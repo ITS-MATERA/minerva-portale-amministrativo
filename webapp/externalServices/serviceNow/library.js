50986858f534b728e06e52f70edf725bfc6c6eb0
Alessio Carone <alessio.carone@innovatesapp.com>
2024-09-04 07:07:16 +0000

    dev: implementata cartella servizio service now!

---
+++ webapp/externalServices/serviceNow/library.js
@@ -0,0 +1,5 @@
+sap.ui.define(["sap/ui/base/ManagedObject", "sap/ui/core/BusyIndicator"], function (ManagedObject, BusyIndicator) {
+  "use strict";
+
+  return ManagedObject.extend("portaleamministrativo.externalServices.serviceNow.library", {});
+});
