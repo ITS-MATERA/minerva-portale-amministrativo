d097ccb02b19072d48464ad2ac86dc43ee99107d
Gianni Lecci <gianni.lecci@innovatesapp.com>
2024-09-05 07:50:07 +0000

    service-now resolve

--- webapp/externalServices/serviceNow/library.js
+++ webapp/externalServices/serviceNow/library.js
@@ -3,19 +3,20 @@ sap.ui.define(["sap/ui/base/ManagedObject", "sap/ui/core/BusyIndicator"], functi
 
   return ManagedObject.extend("portaleamministrativo.externalServices.serviceNow.library", {
     getTicketsList: function (self) {
-      this.getAuthToken(self);
-      return;
+      // this.getAuthToken(self);
+      // return;
 
       var settings = {
         url: this.getUrl(self, "api/sn_customerservice/case/btp_fornitori_get?sysparm_limit=200"),
         method: "GET",
         timeout: 0,
         headers: {
-          Accept: "application/json",
-          Authorization: "",
+          //Accept: "application/json",
+          "Content-Type": "application/json",
+          //"Authorization": "Bearer akvCUJ3zWZ0EXn6ndLOSwM55J6XWa0rciiAgniDTrrkP7ssGK228xxLnfB9p3-CgK8DOyV5ciGzs_Q9-gXo5FA"
         },
       };
-
+      console.log(settings);
       BusyIndicator.show(0);
       $.ajax(settings).done(function (response) {
         BusyIndicator.hide();
@@ -23,46 +24,48 @@ sap.ui.define(["sap/ui/base/ManagedObject", "sap/ui/core/BusyIndicator"], functi
       });
     },
 
-    getAuthToken: function (self) {
-      var settings = {
-        url: this.getUrl(self, "oauth_token.do"),
-        method: "POST",
-        timeout: 0,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        data: {
-          grant_type: "password",
-          client_id: "dd392b0e26f706107706589fbb6b2ba0",
-          client_secret: "3gh5#g#!Vs",
-          username: "btp_user",
-          password: "pIruztPKl[a=jFt){qf*exQQ*1!q_fq[u(KI2le5{>q&BoJVn6-n6H;KpFd-O+X^-?v039T-4Ho.9yG14zF&PLr<X+Gb]Y",
-        },
-      };
-
-      BusyIndicator.show(0);
-      $.ajax(settings).done(function (response) {
-        BusyIndicator.hide();
-        console.log(response);
-      });
-    },
+    // getAuthToken: function (self) {
+    //   var settings = {
+    //     url: this.getUrl(self, "oauth_token.do"),
+    //     method: "POST",
+    //     timeout: 0,
+    //     headers: {
+    //       "Content-Type": "application/x-www-form-urlencoded",
+    //     },
+    //     data: {
+    //       grant_type: "password",
+    //       client_id: "dd392b0e26f706107706589fbb6b2ba0",
+    //       client_secret: "3gh5#g#!Vs",
+    //       username: "btp_user",
+    //       password: "pIruztPKl[a=jFt){qf*exQQ*1!q_fq[u(KI2le5{>q&BoJVn6-n6H;KpFd-O+X^-?v039T-4Ho.9yG14zF&PLr<X+Gb]Y",
+    //     },
+    //   };
+    //   console.log(settings);
+    //   BusyIndicator.show(0);
+    //   $.ajax(settings).done(function (response) {
+    //     BusyIndicator.hide();
+    //     console.log(response);
+    //   });
+    // },
 
     getUrl: function (self, sMethod) {
       var sAppId = self.getOwnerComponent().getMetadata().getManifest()["sap.app"].id;
       var sUrl;
       var sLocation = window.location;
       var sBaseUrl = sLocation.href;
-      var sFlpDestinationurl = jQuery.sap.getModulePath(sAppId + "/service-now");
-
+      var sUrl = jQuery.sap.getModulePath(sAppId + "/service-now");
+      var flpDestinationurl = jQuery.sap.getModulePath(sAppId + "/service-now" + "/");
+      //var sServiceUrl = jQuery.sap.getModulePath(applicationId) + "/go2doc/v2/setDocument"; //byfra
+      //return "https://fstechdev.service-now.com/api/sn_customerservice/case/btp_fornitori_get";
       if (sBaseUrl.includes("workspaces-ws-")) {
-        sUrl = "/service-now" + "/" + sMethod;
+        sUrl = sUrl + "/" + sMethod;
         return sUrl;
       }
 
-      if (sBaseUrl.includes("cp.portal/ui5appruntime.html")) {
-        sUrl = sLocation.protocol + "//" + sLocation.hostname + sFlpDestinationurl + "/" + sMethod;
-        return sUrl;
-      }
+      // if (sBaseUrl.includes("cp.portal/ui5appruntime.html")) {
+      //   sUrl = sLocation.protocol + "//" + sLocation.hostname + sFlpDestinationurl + "/" + sMethod;
+      //   return sUrl;
+      // }
 
       sUrl = sLocation.protocol + "//" + sLocation.hostname + sLocation.pathname;
       sUrl = sUrl.replace("index.html", "");
